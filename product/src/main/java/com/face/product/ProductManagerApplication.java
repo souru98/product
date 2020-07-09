f1b402b56a3b109de07091ec1f8fb01e1793c9da
SourabhAK98 <sourabh@loyalytics.in>
2020-07-09 17:30:24 +0530

    done

---
+++ product/src/main/java/com/face/product/ProductManagerApplication.java
@@ -0,0 +1,15 @@
+package com.face.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductManagerApplication 
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(ProductManagerApplication.class, args);
+	}
+
+}
---
+++ product/src/main/java/com/face/product/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.face.product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product,Long> {
+
+}
